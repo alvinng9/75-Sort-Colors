54f1b33f531eceb994915148df4dd979f4e0afcf
Alvin <alvinngg9@gmail.com>
2024-08-12 18:08:45 -0700

    add comments and tests

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,20 +2,26 @@ package org.example;
 
 public class Solution {
     public void sortColors(int[] nums) {
+        //create 2 pointers at the front and the back of the given array
         int left = 0;
         int right = nums.length - 1;
-        for (int i = 0; i < nums.length && right >= i; i++) {
+        //iterate through the given array up until when i is less than
+        //or equal to the right pointer
+        for (int i = 0; i < nums.length && i <= right; i++) {
+            //if nums[i] equals to 0, swap with the left pointer
             if (nums[i] == 0) {
-                swap(nums, i, left);
+                swap(nums, left, i);
                 left++;
+                //if nums[i] equals to 2, swap with the right pointer
             } else if (nums[i] == 2) {
-                swap(nums, i, right);
+                swap(nums, right, i);
                 right--;
                 i--;
             }
         }
     }
 
+    // function to swap indexes' values in an array
     public void swap(int[] nums, int x, int y) {
         int temp = nums[x];
         nums[x] = nums[y];
